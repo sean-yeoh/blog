7dea32e170ce55bc603f8146a99c61058410788c
Sean Yeoh <sean.yeoh@hotmail.com>
2017-08-07 23:47:02 +0800

    Add posts model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807153955) do
+ActiveRecord::Schema.define(version: 20170807154616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,4 +32,14 @@ ActiveRecord::Schema.define(version: 20170807153955) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "admin_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_posts_on_admin_id"
+  end
+
+  add_foreign_key "posts", "admins"
 end
