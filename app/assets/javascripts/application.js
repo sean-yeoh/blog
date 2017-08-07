a34907690b55c71a2aad59011e4a28c0dfa8cb1c
Sean Yeoh <sean.yeoh@hotmail.com>
2017-08-07 23:37:56 +0800

    Add foundation rails gem. Add posts controller and views.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
