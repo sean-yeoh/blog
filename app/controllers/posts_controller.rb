20eddb3c34b72dfbffa05b17af94043fc165d4ab
Sean Yeoh <sean.yeoh@hotmail.com>
2017-08-08 00:02:37 +0800

    Add posts form. Update posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,39 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def show
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :admin
+  validates :title, :content, presence: true
 end
