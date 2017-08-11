ae4d111fd4357c13bdef8b495c3c5f707103b6ea
Sean Yeoh <sean.yeoh@hotmail.com>
2017-08-12 02:02:26 +0800

    Add styling for post form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :admin, optional: true
-  validates :title, :content, presence: true
+  validates :title, presence: { message: "Title can't be empty." }
+  validates :content, presence: { message: "Content can't be empty." }
   default_scope { order(id: :desc) }
   extend FriendlyId
   friendly_id :yyyy_mm_dd_and_title, use: :slugged
 
   def yyyy_mm_dd_and_title
-    "#{created_at.strftime("%Y %m %d")} #{title}"
+    if created_at && title
+      "#{created_at.strftime("%Y %m %d")} #{title}"
+    end
   end
 
   def should_generate_new_friendly_id?
