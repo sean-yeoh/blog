980147113b7b1dab9854be87d94220d8df67b5c3
sean yeoh <sean.yeoh@hotmail.com>
2017-10-03 22:48:01 +0800

    Precomple ckeditor assets

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Blog
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Kuala Lumpur'
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
