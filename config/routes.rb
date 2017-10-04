c586351d39628745f4f850797e8ae70857b99886
Sean Yeoh <sean.yeoh@hotmail.com>
2017-10-04 10:54:59 +0800

    Add route for admin to edit/update accoutn settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :admins
+  as :admin do
+    get '/settings' => 'devise/registrations#edit', :as => 'admin_settings'
+    put '/settings' => 'devise/registrations#update', :as => 'update_admin_settings'
+  end
   resources :posts, path: "/blog"
   resources :todos do
     member do
