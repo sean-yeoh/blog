4a8bf88402f483f0c80ed9743b4c079a7e6770fc
sean yeoh <sean.yeoh@hotmail.com>
2017-10-04 22:31:32 +0800

    Add loading image. Move quill toolbar to partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,4 +23,63 @@ $(document).on("turbolinks:load", function() {
 		var height = this.scrollHeight - 16
 		$(this).height(height)
 	})
+
+  var Font = Quill.import('formats/font');
+	Font.whitelist = ['arial', 'helvetica'];
+	Quill.register(Font, true);
+
+  var quill = new Quill('#editor-container', {
+    modules: {
+      toolbar: {
+      	container: '#toolbar',
+      	handlers: {
+      		image: imageHandler
+      	}
+      }
+    },
+  	theme: 'snow'
+  });
+
+  $(".post-form").submit(function() {
+		var content = quill.container.firstChild.innerHTML
+		var postContent = $("input[name='post[content]']")
+		console.log(content)
+		postContent.val(content)
+	})
+
+
+  	function imageHandler() {
+  	  var range = this.quill.getSelection();
+  	  const editor = this.quill
+  		const input = document.createElement('input');
+  	  input.setAttribute('type', 'file');
+  	  input.setAttribute('accept', 'image/*');
+  	  input.click();
+  	  input.onchange = () => {
+        const file = input.files[0];
+
+        // file type is only image.
+        if (/^image\//.test(file.type)) {
+          $(".loading").show()
+        	const data = new FormData();
+  	      data.append('file', file);
+
+          $.ajax({
+          	url: "/api/images",
+          	data: data,
+      			cache: false,
+      			contentType: false,
+      			processData: false,
+          	type: "post",
+        		dataType: "json",
+          	success: function(data, status, jqXHR){
+  	  				editor.insertEmbed(range.index, 'image', data.url, Quill.sources.USER);
+              $(".loading").hide()
+          	}
+          })
+        } else {
+          console.warn('You could only upload images.');
+        }
+       };
+  	}
 })
