71a4db4babca165e12be64ed634659039f838f3e
Sean Yeoh <sean.yeoh@hotmail.com>
2017-10-04 18:32:12 +0800

    Authenticate admin for uploading image

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,4 +1,6 @@
 class Api::ImagesController < ApiController
+  before_action :authenticate_admin!
+
 	def create
 		image = Cloudinary::Uploader.upload(params[:file])
 		render json: {url: image["secure_url"]}
